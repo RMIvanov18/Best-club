5b3d9935bc92475c6115ded870962055c5d0672d
BGBinev18 <58163144+BGBinev18@users.noreply.github.com>
2020-06-14 13:53:22 +0300

    Update FINALproject2020.cpp

--- FINALproject2020.cpp
+++ FINALproject2020.cpp
@@ -3,27 +3,23 @@
 
 using namespace std;
 //welcome message function for the whole program
-void WelcomeProgram() {
+void WelcomeProgram()
+{
     cout << endl;
     cout << "Hello! Welcome to our program! There are 3 options. Please choose one and let's start!" << endl;
     cout << endl;
 }
 //welcome message function for the test
 void WelcomeTest()
-
 {
     cout << endl;
     cout << "Test your English. This is a quick English test. There are some sentences and you must put the correct word into the gap by writing it down. At the end of the test you will be given a percentage and an approximation of your level." << endl;
     cout << endl;
-   
-    
-   
-
-
 
 }
 //welcome message function for the game
-void WelcomeGame() {
+void WelcomeGame() 
+{
     cout << endl;
     cout << "Welcome to our quick game!If you are looking for a puzzle that is quick and fun, check out our game!" << endl;
     cout << endl;
@@ -32,9 +28,8 @@ void WelcomeGame() {
 }
 
 //function for choosing an option for the game
-void level() {
-
-
+void level() 
+{
     cout << "Please choose one of the options:" << endl;
 
     cout << endl;
@@ -43,11 +38,10 @@ void level() {
 
     cout << endl;
 
-
 }
 //function for one of the options of the game
-void professions() {
-
+void professions()
+{
 
     string correctword;
     cout << "You chose 'Professions'. Now let's start with the game" << endl;
@@ -77,7 +71,8 @@ void professions() {
 
 }
 //function for the one of the options of the game
-void animals() {
+void animals() 
+{
 
     string correctword;
     cout << "You chose 'Animals'. Now let's start with the game" << endl;
@@ -110,15 +105,10 @@ void animals() {
 
 }
 
-
-
-
-
-
 //function for the game
-void Game() {
+void Game() 
+{
     string choice;
-
     cout << "Hello! You chose play game! In this game you have to guess the words. Let's start!" << endl;
     cout << endl;
     level();
@@ -135,7 +125,8 @@ void Game() {
         professions();
     }
 
-    else {
+    else 
+    {
         cout << "Please choose on of the options(Animals/Professions):" << endl;
         cin >> choice;
     }
@@ -145,140 +136,98 @@ void Game() {
 //function for the questions of the test
 void Questions()
 {
-
-    int counter = 0;
-    string choice;
-    cout << "Question 1" << endl;
+    cout << "Test your English. This is a quick English test. There are some sentences and you must put the correct word into the gap by writing it down. At the end of the test you will be given a percentage and an approximation of your level." << endl;
+    cout << "Welcome to our quiz!" << endl;
     cout << endl;
-    cout << "Tom (is,are,be,am) English" << endl;
+    int counter = 0;//to count a mark of each question
+    string choice;
+    cout << "\nQuestion 1:" << endl;
+    cout << " Tom (is/are/be/am) English" << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "is") {
-
-        counter++;
+    if (choice == "is") //if statement which count +1 point if the user write the correct word
+    {
+        counter++;//+1 point if the user write the correct word
     }
-
-
-
-    cout << "Question 2" << endl;
-    cout << endl;
+    cout << "\nQuestion 2:" << endl;
     cout << " I didn't (watched/watching/watch/not watched) TV last night" << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "watch") {
-
-        counter++;
+    if (choice == "watch")
+    {
+        counter++;//+1 point if the user write the correct word
     }
-
-
-    cout << "Question 3" << endl;
-    cout << endl;
+    cout << "\nQuestion 3:" << endl;
     cout << " (Are/Is/Do/Have) there a restaurant near here?" << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "Is") {
-
-        counter++;
+    if (choice == "Is")
+    {
+        counter++;//+1 point if the user write the correct word
     }
-
-
-    cout << "Question 4" << endl;
-    cout << endl;
+    cout << "\nQuestion 4:" << endl;
     cout << " Hurry up or we'll(lose/miss/fail/catch) our train" << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "miss") {
+    if (choice == "miss")
+    {
 
-        counter++;
+        counter++;//+1 point if the user write the correct word
     }
-
-
-
-    cout << "Question 5" << endl;
-    cout << endl;
-    cout << "My mother(said/told/told to)me to buy some tea." << endl;
+    cout << "\nQuestion 5:" << endl;
+    cout << " My mother(said/told/told to)me to buy some tea." << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "told") {
-
-        counter++;
+    if (choice == "told")
+    {
+        counter++;//+1 point if the user write the correct word
     }
-
-
-
-    cout << "Question 6" << endl;
-    cout << endl;
+    cout << "\nQuestion 6:" << endl;
     cout << " I don't know many people(who/they/which) still smoke nowadays." << endl;
     cout << " Your answer:";
     cin >> choice;
     if (choice == "who") {
 
-        counter++;
+        counter++;//+1 point if the user write the correct word
     }
-
-
-    cout << endl;
-    cout << "Question 7" << endl;
-    cout << endl;
-    cout << "I've never (going/go/went/been) in England" << endl;
+    cout << "\nQuestion 7:" << endl;
+    cout << " I've never (going/go/went/been) in England" << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "been") {
-
-        counter++;
+    if (choice == "been")
+    {
+        counter++;//+1 point if the user write the correct word
     }
-
-
-
-    cout << "Question 8" << endl;
-    cout << endl;
-    cout << "You should(to ask/ask/asked/asking)for directions if you're lost." << endl;
+    cout << "\nQuestion 8:" << endl;
+    cout << " You should(to ask/ask/asked/asking)for directions if you're lost." << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "ask") {
-
-        counter++;
+    if (choice == "ask")
+    {
+        counter++;//+1 point if the user write the correct word
     }
-
-
-
-    cout << "Question 9" << endl;
-    cout << endl;
+    cout << "\nQuestion 9:" << endl;
     cout << " I think I've got a cold, I can't stop(sneeze/sneezing/the sneeze/to sneeze)" << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "sneezing") {
-
-        counter++;
+    if (choice == "sneezing")
+    {
+        counter++;//+1 point if the user write the correct word
     }
-
-
-
-
-    cout << "Question 10" << endl;
-    cout << endl;
-    cout << "You(must/must/must)give us 6" << endl;
+    cout << "\nQuestion 10:" << endl;
+    cout << " You(must/must/must)give us 6" << endl;
     cout << " Your answer:";
     cin >> choice;
-    if (choice == "must") {
-
-        counter++;
+    if (choice == "must")
+    {
+        counter++;//+1 point if the user write the correct word
     }
 
-
-
     int totalScore;
-    totalScore = counter * 10;
-    cout << "Thank you for taking the time to complete this test. You have now reached the end." << endl;
-    cout << "Please click Submit below to see your final score and our assessment of what we think your English level might be." << endl;
-    cout << "3. Submit" << endl;
-    cin >> choice;
-    cout << endl;
-
-    cout << "Your score are:" << totalScore << "%" << endl;
-    cout << endl;
-    cout << endl;
-    cout << "Between 0% and 20% suggests your level is Beginner to Elementary.\n Between 21% and 40% suggests your level is Pre-Intermediate.\n Between 41% and 60% suggests your level is Intermediate.\n Between 61% and 80% suggests your level is Upper-Intermediate.\n Between 81% and 99% suggests your level is Advanced.\n 100% is a perfect score! Your English is excellent." << endl;
+    totalScore = counter * 10;//calculate the percentage
+    cout << "\n\nThank you for taking the time to complete this test. You have now reached the end." << endl;
+    cout << "Your score is:" << totalScore << "%" << endl;
+    cout << "\n\n Between 0% and 20% suggests your level is Beginner to Elementary.\n Between 21% and 40% suggests your level is Pre-Intermediate.\n Between 41% and 60% suggests your level is Intermediate.\n Between 61% and 80% suggests your level is Upper-Intermediate.\n Between 81% and 99% suggests your level is Advanced.\n 100% is a perfect score! Your English is excellent." << endl;
 }
 
 //function for the thirth option-finf word in a text
@@ -321,16 +270,17 @@ void find() {
 }
 
 //function for the Main Menu of the program
-void Menu()
+
+bool showMainMenu()
 {
     int choice;
     cout << "1. Start test" << endl;
     cout << "2. Start game" << endl;
-    cout << "3. Find and replace a word in a text" << endl;
-    cin >> choice;
+    cout << "3. Find word in a sentence" << endl;
+    cout << "0. Exit" << endl;
+    cout << "\nPlease enter your choice: "; cin >> choice;
 
     switch (choice)
-
     {
     case 1:
         WelcomeTest();
@@ -352,14 +302,18 @@ void Menu()
         cout << endl;
         Menu();
         break;
-
+    case 0:
+        return false;
     }
-
 }
+
 int main() {
 
     WelcomeProgram();
-    Menu();
+    bool doShowMenu = true;
+    do {
+        doShowMenu = showMainMenu();
+    } while (doShowMenu);
     system("PAUSE");
     return 1;
 
